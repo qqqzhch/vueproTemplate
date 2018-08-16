8879f52d1de68a8d671f02b01e627dc9c10326ca
zhangchao <544262408@qq.com>
2018-08-16 15:16:06 +0800

    promise error

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+require('es6-promise').polyfill();
+
 import Vue from 'vue'
-import 'promise-polyfill/src/polyfill';
+
 import VueI18n from 'vue-i18n'
 
 import App from './App'
